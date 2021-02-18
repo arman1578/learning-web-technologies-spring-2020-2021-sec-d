648651765b9e744a403375b067478f2259871c5c
arman1578 <armanjoarder1578@gmail.com>
2021-02-18 22:57:25 +0600

    TASK_B

---
+++ MID_LAB_TASK_PHP_05/TASK_2/TASK_B/email.php
@@ -0,0 +1,33 @@
+<?php
+	if(isset($_POST['submit'])){
+
+		$ename = $_POST['myemail'];
+
+		if($ename == ""){
+			echo "null submission...";
+		}else{
+			echo "Email:".$ename;
+
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Email Input</title>
+</head>
+
+<body>
+    <form method="post" action="#">
+        <fieldset>
+            <legend>Email</legend>
+            <input type="text" name="myemail" value=""> <br>
+            <input type="submit" name="submit" value="Submit">
+        </fieldset>
+    </form>
+
+</body>
+
+</html>
