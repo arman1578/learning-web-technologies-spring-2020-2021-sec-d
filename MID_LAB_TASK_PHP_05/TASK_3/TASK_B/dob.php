070377ca5927b9dda3c2866d882ec79cb628b9f3
arman1578 <armanjoarder1578@gmail.com>
2021-02-18 23:25:57 +0600

    TASK_B

---
+++ MID_LAB_TASK_PHP_05/TASK_3/TASK_B/dob.php
@@ -0,0 +1,63 @@
+<?php
+	if(isset($_POST['submit'])){
+
+		$date = $_POST['day'];
+        $month = $_POST['month'];
+        $year = $_POST['year'];
+
+		if($date == "" || $month == ""  || $year == "" ){
+			echo "null submission...";
+		}else{
+			echo "Date:".$date;
+            echo "Month:".$month;
+            echo "Year:".$year;
+		}
+
+	}else{
+		echo "invalid request...";
+	}
+?> 
+
+<html>
+
+<head>
+    <title>Date of Birth Input</title>
+</head>
+
+<body>
+    <form method="post" action=#>
+
+        <fieldset>
+            <legend><b>Date Of Birth</b></legend>
+            <table>
+                <tr>
+                    <td>dd</td>
+                    <td>mm</td>
+                    <td>yyyy</td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <input type="text" name="day" value="">/
+                    </td>
+                    <td>
+                        <input type="text" name="month" value="">/
+                    </td>
+                    <td>
+                        <input type="text" name="year" value="">
+                    </td>
+
+                </tr>
+
+            </table>
+            <hr width="70%" align="left">
+            <input type="Submit" name="submit">
+
+
+        </fieldset>
+
+    </form>
+
+</body>
+
+</html>
